bd100fb0ad4ac1c783e701ce21bc067a8da412bd
Hemant <hy564636@gmail.com>
2025-07-02 21:59:55 +0530

    Add Projects page with scroll detection and dynamic animations; implement Card component

---
+++ Portfolio/src/Componets/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card(img,title,description) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Card
--- Portfolio/src/Pages/Projects.jsx
+++ Portfolio/src/Pages/Projects.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import useScrollDirection from '../Functions/Scrolldetect';
+import Card from '../Componets/Card';
+
 
 function Projects() {
   const direction = useScrollDirection();
   const navigate=useNavigate()
+  const [mounted, setMounted] = useState(false);
+    
+      useEffect(() => {
+        setMounted(true);
+      }, []);
 
 
     useEffect(() => {
@@ -17,10 +24,27 @@ function Projects() {
   }
   }, [direction]);
   return (
-    <div className='text-white'>
-      Projects
-    </div>
-  )
+  <div className="text-white flex flex-col justify-center items-center min-h-screen">
+    <h1
+      className={`text-3xl scale-200 mb-7 font-bold transition-all duration-700
+        ${mounted ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+    >
+      {Array.from("Projects").map((char, i) => (
+        <span
+          key={i}
+          className="inline-block transition-transform duration-400 ease-in-out hover:text-orange-400 hover:animate-bounce"
+          style={{
+            display: char === " " ? "inline-block" : undefined,
+            minWidth: char === " " ? "0.5em" : undefined,
+          }}
+        >
+          {char}
+        </span>
+      ))}
+    </h1>
+    <Card/>
+  </div>
+)
 }
 
 export default Projects
