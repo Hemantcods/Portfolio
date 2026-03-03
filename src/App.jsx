9e80e3011a6c7cd6e08a94d07b6d15581a75eb48
hemantcods <hy564636@gmail.com>
2026-03-03 18:54:04 +0530

    implemented the safari window functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Navbar, Welcome,Dock } from '@components'
 import { Draggable } from 'gsap/Draggable'
 import gsap from 'gsap'
-import {Terminal} from '@windows'
+import {Terminal,Safari} from '@windows'
 gsap.registerPlugin(Draggable)
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <Welcome/>
       <Dock/>
       <Terminal/>
+      <Safari/>
     </main>
   )
 }
